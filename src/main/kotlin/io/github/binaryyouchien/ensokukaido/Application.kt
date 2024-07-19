7082f1be982e94af91ddd11e4f1e7c58ce4807c7
kigawa <kigawa.8390@gmail.com>
2024-07-20 00:02:08 +0900

    add ktor

---
+++ src/main/kotlin/io/github/binaryyouchien/ensokukaido/Application.kt
@@ -0,0 +1,16 @@
+package io.github.binaryyouchien.ensokukaido
+
+import io.github.binaryyouchien.ensokukaido.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureHTTP()
+    configureSecurity()
+    configureRouting()
+}
