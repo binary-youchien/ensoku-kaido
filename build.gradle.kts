bd2b2216b01616188d2eb7040443ea4e6ab201b8
kigawa <kigawa.8390@gmail.com>
2024-07-20 18:23:08 +0900

    remove openapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
-
-val kotlin_version: String by project
-val logback_version: String by project
-val mongo_version: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val mongoVersion: String by project
 
 plugins {
     kotlin("jvm") version "2.0.0"
@@ -13,33 +12,32 @@ group = "io.github.binary-youchien.notepad"
 version = "0.0.1"
 
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+  mainClass.set("io.ktor.server.netty.EngineMain")
 
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+  val isDevelopment: Boolean = project.ext.has("development")
+  applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("org.mongodb:mongodb-driver-core:$mongo_version")
-    implementation("org.mongodb:mongodb-driver-sync:$mongo_version")
-    implementation("org.mongodb:bson:$mongo_version")
-    implementation("io.ktor:ktor-server-swagger-jvm")
-    implementation("io.ktor:ktor-server-openapi")
-    implementation("io.ktor:ktor-server-cors-jvm")
-    implementation("io.ktor:ktor-server-auth-jvm")
-    implementation("io.ktor:ktor-client-core-jvm")
-    implementation("io.ktor:ktor-client-apache-jvm")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+  implementation("io.ktor:ktor-server-core-jvm")
+  implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+  implementation("io.ktor:ktor-server-content-negotiation-jvm")
+  implementation("org.mongodb:mongodb-driver-core:$mongoVersion")
+  implementation("org.mongodb:mongodb-driver-sync:$mongoVersion")
+  implementation("org.mongodb:bson:$mongoVersion")
+  implementation("io.ktor:ktor-server-cors-jvm")
+  implementation("io.ktor:ktor-server-auth-jvm")
+  implementation("io.ktor:ktor-client-core-jvm")
+  implementation("io.ktor:ktor-client-apache-jvm")
+  implementation("io.ktor:ktor-server-auth-jwt-jvm")
+  implementation("io.ktor:ktor-server-netty-jvm")
+  implementation("ch.qos.logback:logback-classic:$logbackVersion")
+  implementation("io.ktor:ktor-server-config-yaml")
+
+  testImplementation("io.ktor:ktor-server-tests-jvm")
+  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
