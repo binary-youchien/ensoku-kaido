5b4947b78008a6dfef5a9a2e85ed3713662a3ade
suga takuya <tqku2003@hotmail.com>
2024-07-27 19:55:02 +0900

    navgation

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,50 +1,113 @@
 import type { MetaFunction } from "@remix-run/node";
-import {Box} from "@mui/system";
+import {Box, Container} from "@mui/system";
 import {Avatar,} from "@mui/material";
 
 import NavItem from '../NavItem';
+import * as React from "react";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "Ensoku Kaido" },
-    { name: "description", content: "Welcome to Remix!" },
+    { name: "description", content: "ensokkaidou!!" },
   ];
 };
 
 export default function Index() {
-  const theme ={
-    navfgColor:"#515B92",
-    navbgColor:"#D9EB99",
-  }
-
   return (
-
-    <div className="font-sans p-4">
-
-      <Box sx={{
-        width: "160px",
-        backgroundColor: '#D9EB99',
-        borderRadius: "30px",
-        boxShadow: '0px 2px 1px rgba(0, 0, 0, 0.2)',
-        height: "90vh",
+    <div className="font-sans p-4"  >
+      <Container sx={{
+        display:"flex",
+        minWidth:"100%",
+        py:4,
+        backgroundColor: '#FBF8FF',
       }}>
         <Box sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: "20%",
-          width: "100%",
+          width:"15%",
+          position:"fixed",
         }}>
-          <Avatar sx={{width: 56, height: 56, mb: 2}}/>
+          <Box sx={{
+            width: "160px",
+            // mx:"auto",
+            backgroundColor: '#D9EB99',
+            borderRadius: "30px",
+            boxShadow: '0px 2px 1px rgba(0, 0, 0, 0.2)',
+            height: "93vh",
+          }}>
+            <Box sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: "20%",
+              width: "100%",
+            }}>
+              <Avatar sx={{width: 56, height: 56, mb: 2}}/>
+            </Box>
+            <Box >
+              <NavItem to="/" fgColor="#515B92" bgColor="#D9EB99">一覧</NavItem>
+              <NavItem to="/" fgColor="#515B92" bgColor="#D9EB99">トップ</NavItem>
+              <NavItem to="/" fgColor="#515B92" bgColor="#D9EB99">ログイン</NavItem>
+            </Box>
+          </Box>
         </Box>
-        <Box >
-          <NavItem to="/" fgColor="#515B92" bgColor="#D9EB99">一覧</NavItem>
-          <NavItem to="/" fgColor="#515B92" bgColor="#D9EB99">トップ</NavItem>
-          <NavItem to="/" fgColor="#515B92" bgColor="#D9EB99">ログイン</NavItem>
+        <Box sx={{
+          width: "85%",
+          marginLeft: "180px",
+        }}>
+
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+          <h1>mainnnnnnnnnnnnn</h1>
+
         </Box>
-      </Box>
 
-
+      </Container>
 
     </div>
   );
