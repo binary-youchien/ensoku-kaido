bb36d314351716e87203e1d602fed663e5710084
kigawa01 <github@kigawa.net>
2024-07-28 05:33:15 +0000

    add title edit

---
+++ app/routes/node/NodeCard.tsx
@@ -0,0 +1,53 @@
+import {Box} from "@mui/system";
+import {BoxTypeMap} from "@mui/system/Box/Box";
+import {OverrideProps} from "@mui/types";
+import {NodeEntity} from "~/routes/node/NodeEntity";
+import {TextInput} from "~/routes/_unit/_form/TextInput";
+import {ErrorMessage} from "~/mui/err/ErrorMessage";
+import {Nodes} from "~/routes/node/Nodes";
+import {taskOrderQueueMap} from "~/entry.client";
+import {NodeClient} from "~/client/nodeClient";
+
+export function NodeCard(
+  {
+    nodeEntity,
+    nodeUpdater,
+    setNodes,
+    ...props
+  }: NodeCardProps,
+) {
+  const res = nodeEntity.data.nodeRes
+  return (
+    <Box
+      {...props}
+      padding={"10px"}
+    >
+      <ErrorMessage error={nodeEntity.data.error?.message}/>
+      {
+        res && <>
+          <TextInput size={"small"} label={"タイトル"} value={res.title} onChange={(event) => {
+            const title = event.target.value
+            setNodes(prev => {
+              prev.nodes[nodeEntity.column].nodeEntities[nodeEntity.row]!.data.nodeRes!!.title = title
+              return prev.copy()
+            })
+            taskOrderQueueMap.dispatch(res.id, async () => {
+              await NodeClient.put(res.roadmapId, res.id, {
+                condition: res?.condition,
+                description: res?.description,
+                downNodeId: res?.downNodeId,
+                rightNodeId: res?.rightNodeId,
+                title: title
+              })
+            })
+          }}/>
+        </>
+      }
+    </Box>
+  )
+}
+
+export interface NodeCardProps extends OverrideProps<BoxTypeMap, any> {
+  nodeEntity: NodeEntity
+  setNodes: (nodeState: (prev: Nodes) => Nodes) => void
+}
--- app/routes/roadmap/RoadmapToolbar.tsx
+++ app/routes/roadmap/RoadmapToolbar.tsx
@@ -5,6 +5,7 @@ import {ApiResult} from "~/client/result";
 import {RoadmapRes} from "~/client/roadmapClient";
 import {Typography} from "@mui/material";
 import {SuccessOrErrMsg} from "~/mui/err/SuccessOrErrMsg";
+import {Link} from "@remix-run/react";
 
 export function RoadmapToolbar(
   {
@@ -16,9 +17,12 @@ export function RoadmapToolbar(
   return (
     <Box
       {...props}
+      display={"flex"}
     >
-      <SuccessOrErrMsg result={roadmapResult} success={result =>
+      <SuccessOrErrMsg result={roadmapResult} success={result => <>
         <Typography variant={"h2"}>{result.value.title}</Typography>
+        <Link to={`/roadmap/${result.value.id}`}>編集完了</Link>
+      </>
       }/>
     </Box>
   )
